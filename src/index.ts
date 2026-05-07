2cd357ef68940e4e223cfd0ab7e98221ba9ebe32
韩耀东 <957673964@qq.com>
2026-05-07 16:33:38 +0800

    fix: 新增useWallet导出

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import ConnectButton from './components/ConnectButton';
 import { Web3Provider } from './context/Web3Context';
+import { useWallet } from './hooks/useWallet';
 
 export {
   ConnectButton,
-  Web3Provider
+  Web3Provider,
+  useWallet,
 };
